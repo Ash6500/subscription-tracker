cec72bfc0505275ef9eb1fb5fff890e61af57ff0
Ash_6500 <ashutosh.rout990@gmail.com>
2025-11-18 22:48:11 +0530

    server created and config complete

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("Hello, Subscription Tracker!");
+import express from 'express';
+import { PORT } from './config/env.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Tracker API!');  
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
