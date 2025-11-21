5bf68e749a4e1477372af8fe8e05964c089a4197
Ash_6500 <ashutosh.rout990@gmail.com>
2025-11-21 17:43:34 +0530

    bot protection and DDoS attack protection feature update

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
 import { PORT } from './config/env.js';
+
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionsRouter from './routes/subscriptions.routes.js'; 
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
-import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(arcjetMiddleware);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
@@ -24,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, async () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Subscription Tracker is running on http://localhost:${PORT}`);
 
     await connectToDatabase();
 });
